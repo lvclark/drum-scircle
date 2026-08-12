2801196160b3cc3a0872fc2b309df288bc0d9c76
Lindsay Clark <Lindsay.Clark@seattlechildrens.org>
2026-08-11 21:05:11 -0700

    It's alive

--- R/drum_scircle.R
+++ R/drum_scircle.R
@@ -52,6 +52,36 @@ order_umaps <- function(umap_list){
     return(umap_list[path])
 }
 
+# Build long-format data frame for ggplot
+prep_gg <- function(umap_list, color_by){
+    n <- length(umap_list)
+    df_list <- vector(mode = "list", length = n)
+    for(i in seq_len(n)){
+        df_list[[i]] <- data.frame(
+            UMAP_1 = umap_list[[i]][,1],
+            UMAP_2 = umap_list[[i]][,2],
+            Color = color_by
+        )
+        df_list[[i]]$Iteration <- i
+        if(is.null(rownames(umap_list[[i]]))){
+            df_list[[i]]$Sample <-
+                paste0("sample", seq_len(nrow(umap_list[[i]])))
+        } else {
+            df_list[[i]]$Sample <- rownames(umap_list[[i]])
+        }
+    }
+    return(do.call(rbind, df_list))
+}
+
+# Build plot
+plot_gg <- function(df){
+    p <- ggplot2::ggplot(df, ggplot2::aes(x = UMAP_1, y = UMAP_2, color = Color)) +
+        ggplot2::geom_point() +
+        gganimate::transition_time(Iteration) +
+        gganimate::ease_aes("linear")
+    return(p)
+}
+
 # Testing
 sample_meta <- read.csv("../../SIDS/ramirez_2025-12_downstream/popstruct/sample_metadata_2025-12-16.csv")
 x <- as.matrix(sample_meta[,c("PC1", "PC2", "PC3")])
@@ -59,6 +89,11 @@ x <- as.matrix(sample_meta[,c("PC1", "PC2", "PC3")])
 u <- make_umaps(x)
 r <- rotate_umaps(u)
 o <- order_umaps(r)
+pr <- prep_gg(o, sample_meta$Q_African)
+
+p <- plot_gg(pr) + labs(color = "African ancestry")
+
+p
 
 library(ggplot2)
 
